871cac045cd1b4dc16e91ecba892ed119c493436
Pete Connor <peteconnor@petes-mbp.fios-router.home>
2019-02-27 22:36:54 -0500

    added hud, & motion to bounce

--- MiniGames!/Bounce/GameScene_bounce.swift
+++ MiniGames!/Bounce/GameScene_bounce.swift
@@ -7,6 +7,7 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 class GameScene_bounce: SKScene, SKPhysicsContactDelegate {
     
@@ -68,18 +69,54 @@ class GameScene_bounce: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    var player = SKSpriteNode()
+    var obstacle = SKSpriteNode()
+    var xMotion: CGFloat = 0
+    let motionManager = CMMotionManager()
+    let cam = SKCameraNode()
+    
 
     override func didMove(to view: SKView) {
         
         numAtlas.preload {
         }
         self.physicsWorld.contactDelegate = self
+        self.camera = cam
+        self.addChild(cam)
         
         NotificationCenter.default.addObserver(self, selector: #selector(GameScene_bounce.pauseGame), name: NSNotification.Name(rawValue: "PauseGame"), object: nil)
         
         addPauseButton()
         addBackButton()
         addScoreLabels()
+        addPlayer()
+        addPlayerBlurr()
+        addObstacle()
+        
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data: CMAccelerometerData?, error: Error?) in
+            if let accelerometerData = data {
+                let acceleration = accelerometerData.acceleration
+                self.xMotion = CGFloat(acceleration.x) * (self.size.width * 0.04)
+            }
+        }
+    }
+    
+    override func didSimulatePhysics() {
+        if isGamePaused == false {
+            player.position.x += xMotion
+            
+            if player.position.x < 0 {
+                player.position = CGPoint(x: 0, y: player.position.y)
+            } else if player.position.x > self.size.width {
+                player.position = CGPoint(x: self.size.width, y: player.position.y)
+            }
+        }
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        cam.position = player.position
     }
     
     
@@ -95,19 +132,15 @@ class GameScene_bounce: SKScene, SKPhysicsContactDelegate {
     
     func addPauseButton() {
         pauseButton = SKSpriteNode(texture: SKTexture(imageNamed: "PauseButton"))
-        pauseButton.name = "PauseButton"
-        //pauseButton.size.width = 42.7
-        //pauseButton.size.height = 75
-        
-        pauseButton.zPosition = 6
-        addChild(pauseButton)
-        
         pauseButtonBlurr = SKSpriteNode(imageNamed: "GreenPauseButtonBlurr")
-        //pauseButtonBlurr.size = CGSize(width: 72.4, height: 104.7)
+        pauseButton.name = "PauseButton"
+        pauseButton.position = CGPoint(x: 325 - pauseButtonBlurr.size.width/2 - 25, y: 667 - pauseButtonBlurr.size.height/2 - 25)
+        pauseButton.zPosition = 6
+        cam.addChild(pauseButton)
         pauseButton.addChild(pauseButtonBlurr)
         pauseButtonBlurr.zPosition = -1
         
-        pauseButton.position = CGPoint(x: 750 - pauseButtonBlurr.size.width/2 - 25, y: 1334 - pauseButtonBlurr.size.height/2 - 25)
+        
         
     }
     
@@ -116,28 +149,28 @@ class GameScene_bounce: SKScene, SKPhysicsContactDelegate {
         backButton.name = "BackButton"
         //backButton.size = CGSize(width: 32.3, height: 75)
         backButton.zPosition = 6
-        addChild(backButton)
+        cam.addChild(backButton)
         
         let backButtonBlurr = SKSpriteNode(imageNamed: "RedBackButtonBlurr")
         //ZbackButtonBlurr.size = CGSize(width: 67.2, height: 115.8)
         backButton.addChild(backButtonBlurr)
         backButtonBlurr.zPosition = -1
         
-        backButton.position = CGPoint(x: 0 + backButtonBlurr.size.width/2 + 25, y: 1334 - backButtonBlurr.size.height/2 - 25)
+        backButton.position = CGPoint(x: -325 + backButtonBlurr.size.width/2 + 25, y: 667 - backButtonBlurr.size.height/2 - 25)
         
     }
     
     func addScoreLabels() {
         scoreLabel1.zPosition = 3
-        scoreLabel1.position = CGPoint(x: self.size.width/2 - scoreLabel1.size.width - 20, y: 1334 - scoreLabel1.size.height/2 - 40)
+        scoreLabel1.position = CGPoint(x: -scoreLabel1.size.width - 20, y: 667 - scoreLabel1.size.height/2 - 40)
         scoreLabel2.zPosition = 3
-        scoreLabel2.position = CGPoint(x: self.size.width/2, y: 1334 - scoreLabel2.size.height/2 - 40)
+        scoreLabel2.position = CGPoint(x: 0, y: 667 - scoreLabel2.size.height/2 - 40)
         scoreLabel3.zPosition = 3
-        scoreLabel3.position = CGPoint(x: self.size.width/2 + scoreLabel3.size.width + 20, y: 1334 - scoreLabel3.size.height/2 - 40)
+        scoreLabel3.position = CGPoint(x: scoreLabel3.size.width + 20, y: 667 - scoreLabel3.size.height/2 - 40)
         
-        addChild(scoreLabel1)
-        addChild(scoreLabel2)
-        addChild(scoreLabel3)
+        cam.addChild(scoreLabel1)
+        cam.addChild(scoreLabel2)
+        cam.addChild(scoreLabel3)
         
         
         
@@ -150,7 +183,50 @@ class GameScene_bounce: SKScene, SKPhysicsContactDelegate {
         blurr3.zPosition = -1
     }
     
+    func addPlayer() {
+        player = SKSpriteNode(imageNamed: "Disc")
+        player.position = CGPoint(x: self.size.width/2, y: 350)
+        player.name = "PLAYER"
+        player.physicsBody?.isDynamic = false
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        player.physicsBody?.categoryBitMask = 1
+        player.physicsBody?.collisionBitMask = 0
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        player.zPosition = 3
+        player.physicsBody?.isDynamic = false
+    }
+    
+    func addPlayerBlurr() {
+        let playerBlurr = SKSpriteNode(imageNamed: "GreenDiscBlurr")
+        //playerBlurr.position = CGPoint(x: player.position.x, y: player.position.y)
+        //playerBlurr.name = "PLAYER"
+        //playerBlurr.physicsBody?.isDynamic = false
+        //playerBlurr.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        //playerBlurr.physicsBody?.categoryBitMask = CollisionBitMask_evade.Player
+        //playerBlurr.physicsBody?.collisionBitMask = 0
+        //playerBlurr.physicsBody?.contactTestBitMask = CollisionBitMask_evade.Obstacle
+        player.addChild(playerBlurr)
+        playerBlurr.zPosition = -1
+    }
+    
+    func addObstacle() {
+        obstacle = SKSpriteNode(imageNamed: "Disc")
+        obstacle.position = CGPoint(x: self.size.width/2, y: 200)
+        obstacle.name = "OBSTACLE"
+        obstacle.physicsBody?.isDynamic = false
+        obstacle.physicsBody = SKPhysicsBody(circleOfRadius: obstacle.size.width/2)
+        obstacle.physicsBody?.categoryBitMask = 1
+        obstacle.physicsBody?.collisionBitMask = 0
+        obstacle.physicsBody?.contactTestBitMask = 1
+        addChild(obstacle)
+        obstacle.zPosition = 3
+        obstacle.physicsBody?.isDynamic = false
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        player.position.y += 10
         if started && !isPaused {
             //canMove = true - from evade
         }
